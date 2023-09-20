78e8a364e648857ce4e5860a4143c0832d14930e
jb6131 <44068903+jb6131@users.noreply.github.com>
2023-09-20 18:53:14 -0500

    added login/logout routes and js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,13 @@
 const router = require('express').Router();
 
+// GET route to render login page
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// GET route to render signup page
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
 
 module.exports = router;
