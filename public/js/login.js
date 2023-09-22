1167ef909798aa1a326e72cc68def0c03b08fb8c
Caleb Wood <Calebwjwood60@gmail.com>
2023-09-21 21:10:42 -0500

    added loginJS

--- public/js/login.js
+++ public/js/login.js
@@ -1,12 +1,11 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
+ 
 
-  // Collect values from the login form
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
-
+  const email = document.querySelector('#login-email').value.trim();
+  const password = document.querySelector('#login-pwd').value.trim();
+  
   if (email && password) {
-    // Send a POST request to the API endpoint
     const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
@@ -14,7 +13,6 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      // If successful, redirect the browser to the profile page
       document.location.replace('/profile');
     } else {
       alert(response.statusText);
@@ -22,6 +20,7 @@ const loginFormHandler = async (event) => {
   }
 };
 
-document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
+const loginBtn = document.getElementById('login-btn')
+
+
+loginBtn.addEventListener('click', loginFormHandler);
