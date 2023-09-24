e479fade85befaff5d597fdd96c077a09dfec431
jb6131 <44068903+jb6131@users.noreply.github.com>
2023-09-24 11:29:51 -0500

    added profile and create/delete jobs

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const { User, Job, Tips } = require('../models');
+const withAuth = require('../utils/auth');
+
 // GET route to render the homepage
 router.get('/', (req, res) => {
   
@@ -6,6 +9,25 @@ router.get('/', (req, res) => {
     logged_in: req.session.logged_in
   })
 })
+
+// GET route to render profile page
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Job }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // GET route to render login page
 router.get('/login', (req, res) => {
